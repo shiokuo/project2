657de03ecb2ddea123f48b28f4ef72a082c4de83
henry <b08901032@ntu.edu.tw>
2019-11-17 14:55:58 +0800

    acc 0.1

--- pj2.py
+++ pj2.py
@@ -33,6 +33,7 @@ plt.ylim([48.747,48.754])
 ax.set_zlim([-20,40])
 plt.colorbar(bar)
 data=data[0::8,:]
+print(data)
 x=data[:,1]
 y=data[:,0]
 z=data[:,2]
@@ -40,6 +41,16 @@ t=data[:,3]
 bar=ax.scatter3D(x,y,z,c='green',s=(t-120800)/20,alpha=0.8)
 ax.set_title('route mapping')
 #劉先生
+
+for index in range(1,8,1):
+    acc=np.array(data[index-1,:]+data[index+1,:]-2*data[index,:])
+    linedot=[]
+    for dot in range(1,5):
+        linedot.append(data[index,:]+3*dot*acc[:]/5)
+    acc_draw=np.array(linedot)
+    #print(acc_draw)
+    ax.plot3D(acc_draw[:,1],acc_draw[:,0],acc_draw[:,2],color='black')
+
 plt.show()
 
 
